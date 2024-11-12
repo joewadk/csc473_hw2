ee2e4d1fcb22f91e6493e60a1f6df14de6b2af09
joewadk <jkabir000@citymail.cuny.edu>
2024-11-12 05:00:12 -0500

    empty response handler added

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -4,6 +4,7 @@ const URL = 'https://restaurant-inspection-api-40554916dc60.herokuapp.com'; //ba
 const SearchComponent = () => {
   const [restaurantName, setRestaurantName] = useState(''); //state for restaurant name
   const [results, setResults] = useState({ data: [] }); //state for results
+  const [searched, setSearched] = useState(false); //state for search 
 
   const handleInputChange = (event) => {
     setRestaurantName(event.target.value);//setting restaurant name to the value of the input
@@ -18,6 +19,7 @@ const SearchComponent = () => {
       console.error('Error fetching data:', error); //error catching
       setResults({});
     }
+    setSearched(true); //setting searched to true
   };
 
   return (//structured output. include a box for the output json. also used <br> to ensure search button and search box are on diff lines
@@ -31,11 +33,15 @@ const SearchComponent = () => {
         <br /> 
       <button onClick={handleSearch}>Search</button>
       <div>
-        {results.data.map((entry, index) => (//mapping over the results and displaying them in a box
+        {//applying the conditional here to check response length ==0, if it is then print
+            searched && results.data.length === 0 ? (
+                <div>No results found.</div>
+            ) : (
+        results.data.map((entry, index) => (//mapping over the results and displaying them in a box
           <div key={index} style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
             <pre>{JSON.stringify(entry, null, 2)}</pre>
           </div>
-        ))}
+        )))} 
       </div>
     </div>
   );
